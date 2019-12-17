0388b26f56dde9f997b412e2bd800bc8e8246ac0
Gasan Alkadarskiy <alkadarskiy.off@gmail.com>
2019-12-17 19:51:56 +0300

    Added 4 and 5 lab works

---
+++ Lab5/lab4/lab4/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.Chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbx_inform = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart1.ChartAreas.Add(chartArea1);
+            this.Chart1.Cursor = System.Windows.Forms.Cursors.Default;
+            legend1.Name = "Legend1";
+            this.Chart1.Legends.Add(legend1);
+            this.Chart1.Location = new System.Drawing.Point(3, 1);
+            this.Chart1.Name = "Chart1";
+            this.Chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Point";
+            this.Chart1.Series.Add(series1);
+            this.Chart1.Size = new System.Drawing.Size(270, 231);
+            this.Chart1.TabIndex = 0;
+            this.Chart1.Text = "Распределение памяти";
+            title1.Name = "Title1";
+            title1.Text = "Физическая память";
+            this.Chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Полная информация о памяти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbx_inform
+            // 
+            this.lbx_inform.FormattingEnabled = true;
+            this.lbx_inform.Location = new System.Drawing.Point(562, 64);
+            this.lbx_inform.Name = "lbx_inform";
+            this.lbx_inform.Size = new System.Drawing.Size(369, 355);
+            this.lbx_inform.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(595, 442);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(324, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Построить карту процессов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Cursor = System.Windows.Forms.Cursors.Default;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(3, 257);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Point";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(270, 231);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "Файл подкачки";
+            title2.Name = "Title1";
+            title2.Text = "Файл подкачки";
+            this.chart2.Titles.Add(title2);
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Cursor = System.Windows.Forms.Cursors.Default;
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(286, 136);
+            this.chart3.Name = "chart3";
+            this.chart3.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.Legend = "Legend1";
+            series3.Name = "Point";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(270, 231);
+            this.chart3.TabIndex = 5;
+            this.chart3.Text = "Распределение памяти";
+            title3.Name = "Title1";
+            title3.Text = "Виртуальная память";
+            this.chart3.Titles.Add(title3);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 500);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbx_inform);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbx_inform;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ Lab5/lab4/lab4/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace lab4 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            List<double> yValues = new List<double>() { 55.62, 45.54, 73.45, 9.73, 88.42, 45.9, 63.6, 85.1, 67.2, 23.6 };
+
+            MemoryStatus status = MemoryStatus.CreateInstance();
+            uint MemoryLoad = status.MemoryLoad;
+            ulong TotalPhys = status.TotalPhys;
+            ulong AvailPhys = status.AvailPhys;
+            ulong TotalPageFile = status.TotalPageFile;
+            ulong AvailPageFile = status.AvailPageFile;
+            ulong TotalVirtual = status.TotalVirtual;
+            ulong AvailVirtual = status.AvailVirtual;
+            ulong AvailExtendedVirtual = status.AvailExtendedVirtual;
+            lbx_inform.Items.Add("Всего физической памяти= " + TotalPhys / 1024 / 1024 + " MB");
+            lbx_inform.Items.Add("Объем физической памяти, доступный в данный момент " + AvailPhys / 1024 / 1024 + " MB");
+            lbx_inform.Items.Add("Всего файл подкачки= " + TotalPageFile / 1024 / 1024 + " MB");
+            lbx_inform.Items.Add("Объем файла подкачки, доступный в данный момент " + AvailPageFile / 1024 / 1024 + " MB");
+            lbx_inform.Items.Add("Всего виртуальной памяти= " + TotalVirtual / 1024 / 1024 + " MB");
+            lbx_inform.Items.Add("Объем виртуальной памяти, доступный в данный момент " + AvailVirtual / 1024 / 1024 + " MB");
+            lbx_inform.Items.Add("Используется памяти данным процессом: " + MemoryLoad + "байт");
+            List<ulong> statuses = new List<ulong>() { (TotalPhys-AvailPhys), AvailPhys };
+             List<ulong> statuses1 = new List<ulong>() { (TotalPageFile-AvailPageFile), AvailPageFile };
+            List<ulong> statuses2 = new List<ulong>() { (TotalVirtual-AvailVirtual), AvailVirtual };
+            Chart1.Series["Point"].Points.DataBindY(statuses);
+            
+            chart2.Series["Point"].Points.DataBindY(statuses1);
+            chart3.Series["Point"].Points.DataBindY(statuses2);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            System.Diagnostics.Process MyProc = new System.Diagnostics.Process();
+            MyProc.StartInfo.FileName = @"C:\Users\gasan\source\repos\OS_lab4\Debug\OS_lab4.exe";
+            MyProc.Start();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+
+    public class MemoryStatus {
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern bool GlobalMemoryStatusEx([In, Out] MemoryStatus lpBuffer);
+
+        private uint dwLength;
+        public uint MemoryLoad;
+        public ulong TotalPhys;
+        public ulong AvailPhys;
+        public ulong TotalPageFile;
+        public ulong AvailPageFile;
+        public ulong TotalVirtual;
+        public ulong AvailVirtual;
+        public ulong AvailExtendedVirtual;
+
+        private static volatile MemoryStatus singleton;
+        private static readonly object syncroot = new object();
+        public static MemoryStatus CreateInstance() {
+            if (singleton == null)
+                lock (syncroot)
+                    if (singleton == null)
+                        singleton = new MemoryStatus();
+            return singleton;
+        }
+
+        [SecurityCritical]
+        private MemoryStatus() {
+            dwLength = (uint)Marshal.SizeOf(typeof(MemoryStatus));
+            GlobalMemoryStatusEx(this);
+        }
+    }
+}
